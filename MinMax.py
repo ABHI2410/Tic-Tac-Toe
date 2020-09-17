a523490b46be1bf33426607b2a026439d79b78e5
ABHI2410 <patel.ag@somaiya.edu>
2020-09-17 11:59:10 +0530

    Added comments

--- MinMax.py
+++ MinMax.py
@@ -1,26 +1,30 @@
 class Board():
 
-
+	####################################################################
+	#                  Defining all important variables                #
+	####################################################################
 	def __init__(self):
 
 		self.board = [
 				['','',''],
 				['','',''],
 				['','',''],
-			]								
-		self.winner = None					
-		self.currentPlayer = 'AIPlayer'		
-		self.scores = {						 
+			]								# tic-tac-toe board
+		self.winner = None					# tic-tac-toe winner
+		self.currentPlayer = 'AIPlayer'		# Player who is playing
+		self.scores = {						# Score of each outcome 
 			'X':1,
 			'O':-1,
 			'tie':0,
 		}
 
-		self.ai = 'X'						
-		self.human = 'O'					
+		self.ai = 'X'						# AI player Initial
+		self.human = 'O'					# Human player Initial
 
 
-	
+	####################################################################
+	#                  Checking if board is full or not                #
+	####################################################################
 	def isBoardNotFull(self):
 		for i in range (len(self.board)):
 				for j in range (len(self.board[i])):
@@ -32,7 +36,9 @@ class Board():
 		return Ans
 
 
-
+	####################################################################
+	#           Checking if board position is full or not              #
+	####################################################################
 	def checkSpaceOnBoard(self,place):
 		if place == '':
 			return True
@@ -40,9 +46,12 @@ class Board():
 			return False
 
 
+	####################################################################
+	#              Checking if any player has won yet                  #
+	####################################################################
 	def checkWinner(self):
 		self.winner = None
-		
+		#Diagonal win condition check
 		if self.board[2][0] == self.board[1][1] and self.board[1][1] == self.board[0][2]:
 			self.winner = self.board[2][0]
 		elif self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[2][2]:
@@ -51,71 +60,78 @@ class Board():
 
 			for i in range(len(self.board)):
 				if self.winner in ["", None]:
-					
+					#horizontal win condition check
 					if self.board[i][0] == self.board[i][1] and self.board[i][1] == self.board[i][2]:
 						self.winner = self.board[i][0]
-					
+					#vertical win condition check
 					elif self.board[0][i] == self.board[1][i] and self.board[1][i] == self.board[2][i]:
 						self.winner = self.board[0][i]
 
 
 		if self.winner == None and self.isBoardNotFull() == False:
-			return 'tie'						
+			return 'tie'						# Return Tie if Board is full and no winner was found
 		elif self.winner == None:
-			return ""							
+			return ""							# Return null if no winner is found
 		else:
-			return self.winner 					
+			return self.winner 					# Return winner if found
 
 
-	
+	####################################################################
+	#                Recursive MinMax Algorithm                        #
+	####################################################################
 	def minmax(self,depth,isMaxinizig):
 		result = self.checkWinner()
 
-		
+		# winner found return score
 		if result != '':
 			return self.scores[result]
 
-		 
+		# AI Player's turn 
 		if isMaxinizig:
 			bestscore = -1
 			for i in range (len(self.board)):
 				for j in range (len(self.board[i])):
 					if self.checkSpaceOnBoard(self.board[i][j]):
-						self.board[i][j] = self.ai 				
-						score = self.minmax(depth+1,False)		
+						self.board[i][j] = self.ai 				# AI Initial Assigning
+						score = self.minmax(depth+1,False)		# Recursive Call for minmax
 						self.board[i][j] = '';
-						bestscore = max(score,bestscore)		
-		
+						bestscore = max(score,bestscore)		# Check if move was optimal or not
+		# Human Player's turn
 		else:
 			bestscore = 1
 			for i in range (len(self.board)):
 				for j in range (len(self.board[i])):
 					if self.checkSpaceOnBoard(self.board[i][j]):
-						self.board[i][j] = self.human 			
-						score = self.minmax(depth+1,True)		
+						self.board[i][j] = self.human 			# Human Initial Assigning
+						score = self.minmax(depth+1,True)		# Recursive Call for minmax
 						self.board[i][j] = '';
-						bestscore = min(score,bestscore) 		
+						bestscore = min(score,bestscore) 		# Check if move was optimal or not
 		return bestscore
 
 
+	####################################################################
+	#                Finding Best Move for AI                          #
+	####################################################################
 	def bestMove(self):
 		bestscore = -1000000
 		for i in range (len(self.board)):
 				for j in range (len(self.board[i])):
 					if self.checkSpaceOnBoard(self.board[i][j]):
-						self.board[i][j] = self.ai 				
-						score = self.minmax(0,False)			
+						self.board[i][j] = self.ai 				# Human Initial Assigning
+						score = self.minmax(0,False)			# Function call for minmax
 						self.board[i][j] = ''
-						if  score > bestscore:					
+						if  score > bestscore:					# Check if move is best move or not
 							bestscore = score
 							bestMove = [i,j]
 		a = bestMove[0]
 		b = bestMove[1]
-		self.board[a][b] = self.ai 								 
-		self.currentPlayer = 'HumanPlayer'						
+		self.board[a][b] = self.ai 								# Play the best move 
+		self.currentPlayer = 'HumanPlayer'						# Pass the Game to Human Player
 
 
-
+	####################################################################
+	#            Representation of Tic-Tac-Toe board                   #
+	####################################################################
 	def __repr__(self):
 		out = ""
 		for i in self.board:
@@ -127,23 +143,25 @@ class Board():
 		return out
 
 
-
+	####################################################################
+	#                Main Drive Function                               #
+	####################################################################
 if __name__ == '__main__':
 	print("Let the Game Begin")
-	board = Board()													
-	while board.isBoardNotFull():									
-		if board.currentPlayer == 'AIPlayer':						
+	board = Board()													# Creating and object
+	while board.isBoardNotFull():									# Continue playing till the board is full
+		if board.currentPlayer == 'AIPlayer':						# Call bestMove function for AIPlayer
 			board.bestMove()
 			
-		else:														
-			currentwinner=board.checkWinner()						 
-			if board.isBoardNotFull() and currentwinner != None:	
-				row = int(input("Enter row of next move"))			
-				column = int(input("Enter column of next move"))	
-				board.board[row][column] = board.human 				
-				board.currentPlayer = 'AIPlayer' 					
+		else:														# Human's Turn
+			currentwinner=board.checkWinner()						# Check for winner 
+			if board.isBoardNotFull() and currentwinner != None:	# Play if board is not full and winner is not obtained
+				row = int(input("Enter row of next move"))			# Y-coordinates of Human move 
+				column = int(input("Enter column of next move"))	# X-coordinates of Human move 
+				board.board[row][column] = board.human 				# Play Human move
+				board.currentPlayer = 'AIPlayer' 					# Change to AIPlayer 
 		print(board)	
-		if board.checkWinner() != '':								
+		if board.checkWinner() != '':								# Check for Winner 
 			result = board.checkWinner() 						
 			if result != None:
 				ans = board.scores[result]
@@ -156,6 +174,6 @@ if __name__ == '__main__':
 			break
 
 
-	print("Winner is: ",board.winner)								
+	print("Winner is: ",board.winner)								# Declare the winner 
 
 		
