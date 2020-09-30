1bcb82fc06b8520b0cbcc6c47ef8f658092ab283
ABHI2410 <patel.ag@somaiya.edu>
2020-09-30 15:37:49 +0530

    Added comments and updated readme.md file

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,28 @@
 from .Minmax import MinMaxUi
+from .Minmax import MinMax
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 if __name__ == "__main__":
     import sys
+
+    ## Starting the QT5based application 
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = MinMaxUi.Ui_MainWindow()
+
+    ## Creating Board object
+    board = MinMax.Board()
+
+    ## Creating MinMaxUi object
+    ui = MinMaxUi.Ui_MainWindow(board)
+
+    ## Creating the layout 
     ui.setupUi(MainWindow)
+
+    ## Displaying the layout 
     MainWindow.show()
+
+    ## Starting the game
+    ui.start()
+
+    ##Exit
     sys.exit(app.exec_())
